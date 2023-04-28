226e01a5221daba2f5e5c1a79035535e91a77e30
Tasneem Iqbal <tasneemiqbal417@gmail.com>
2023-04-28 00:19:40 -0700

    Update MainActivity.kt

--- app/src/main/java/com/example/carbonator/MainActivity.kt
+++ app/src/main/java/com/example/carbonator/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+<<<<<<< Updated upstream
+=======
+
+        //to go to the next activity/page
+        val calculationActivity = findViewById<Button>(R.id.button1)
+        calculationActivity.setOnClickListener {
+            val intent = Intent(this,Calculations::class.java) //you pass the class
+            startActivity(intent)
+        }
+//<<<<<<< Updated upstream
+        getCO2e()
+        val co2e = findViewById<TextView>(R.id.co2e)
+        co2e.setText(num.toString())
+>>>>>>> Stashed changes
     }
 }
