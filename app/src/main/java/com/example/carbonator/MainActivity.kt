a14a02b145575b2d030693b691db0d0ea6ab07ed
TedmanNguyen <97199766+TedmanNguyen@users.noreply.github.com>
2023-04-28 04:04:32 -0700

    Api call for Vehicles
    The API will now call a list of vehicles and report various information. Recycler view needs to be implemented next.

--- app/src/main/java/com/example/carbonator/MainActivity.kt
+++ app/src/main/java/com/example/carbonator/MainActivity.kt
@@ -1,46 +1,48 @@
 package com.example.carbonator
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//<<<<<<< Updated upstream
 import android.util.Log
 import android.widget.TextView
-
 import com.codepath.asynchttpclient.RequestHeaders
 import com.codepath.asynchttpclient.RequestParams
-
-
 import okhttp3.Headers
-
-//=======
 import android.widget.Button
-
-//>>>>>>> Stashed changes
 
 
 class MainActivity : AppCompatActivity() {
     var num: Double = 0.0
+    data class carInfoPost(val postmake: String, val postmodel: String, val postyear: Int, val postemission: Int)
+    private lateinit var carList: MutableList<String>
+    private lateinit var carListPost: MutableList<carInfoPost>
+    var make = ""
+    var model = ""
+    var year = 0
+    var emission = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val calculationActivity = findViewById<Button>(R.id.button1)
         calculationActivity.setOnClickListener {
             val intent = Intent(this,Calculations::class.java) //you pass the class
             startActivity(intent)
         }
-//<<<<<<< Updated upstream
-        getCO2e()
+        createCarList()
+
+        for (i in 0 until carList.size) {
+            getCO2e(i)
+        }
         val co2e = findViewById<TextView>(R.id.co2e)
         co2e.setText(num.toString())
     }
 
-    private fun getCO2e() {
+    private fun getCO2e(i: Int) {
         val client = com.codepath.asynchttpclient.AsyncHttpClient()
         val url = "https://www.carboninterface.com/api/v1/estimates"
         val apiKey = "YcTh6ZXIQoTvQCCw1sew"
+        val carString = carList[i]
 
         val requestHeaders = RequestHeaders()
         requestHeaders["Authorization"] = "Bearer YcTh6ZXIQoTvQCCw1sew"
@@ -49,11 +51,11 @@ class MainActivity : AppCompatActivity() {
 
         val requestBody = """
     {
-        "type": "electricity",
-        "electricity_unit": "mwh",
-        "electricity_value": 42,
-        "country": "us",
-        "state": "fl"
+        "type": "vehicle",
+        "distance_unit": "mi",
+        "distance_value": 15000,
+        "vehicle_model_id": "$carString"
+
     }
     """.trimIndent()
 
@@ -67,8 +69,18 @@ class MainActivity : AppCompatActivity() {
                 headers: Headers,
                 json: com.codepath.asynchttpclient.callback.JsonHttpResponseHandler.JSON
             ) {
-                num = json.jsonObject.getJSONObject("data").getJSONObject("attributes").getDouble("carbon_kg")
-                Log.d("co2e", "response successful")
+
+                make = json.jsonObject.getJSONObject("data").getJSONObject("attributes").getString("vehicle_make")
+                Log.d("make", make)
+                model = json.jsonObject.getJSONObject("data").getJSONObject("attributes").getString("vehicle_model")
+                Log.d("model", model)
+                year = json.jsonObject.getJSONObject("data").getJSONObject("attributes").getInt("vehicle_year")
+                Log.d("year", year.toString())
+                emission = json.jsonObject.getJSONObject("data").getJSONObject("attributes").getDouble("carbon_kg")
+                Log.d("emission", emission.toString())
+                //carListPost.add(carInfoPost(make, model, year, emission))
+
+                //Log.d("Poke list", carListPost.toString())
             }
 
             override fun onFailure(
@@ -81,9 +93,22 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+    private fun createCarList() {
+        carList = mutableListOf(
+            "79d22090-4378-41b5-befc-79602216476e",
+            "4edb1248-f0bc-4ed2-9b40-4592c9b1c287",
+            "4d26cff2-0b7a-44e3-b416-3299f97bba4e",
+            "70c2fe22-6f3e-4f15-a3ca-839e70dfe8e9",
+            "e3ced1d5-f2ad-4baa-a9c2-e94a8d1fc280",
 
+        )
+
+    }
 }
-
-
+//"e6d9e81c-62e2-4cca-9173-407ae8c22624",
+//"1d2e250f-01c4-4966-ad02-1f98380e9113",
+//"4c979440-64f9-4c2e-9d8e-8800c9239044",
+//"89f885e6-2016-4080-ad39-3396cd559f60",
+//"286a9817-a619-44cb-ac78-c7a321c5867d",
 
 
